4b3fd181ed676c60bdb6d69926f2910ac59c03a4
Shreyash-jiwane09 <shrey.jiwane09@gmail.com>
2025-11-12 10:09:56 +0530

    add setup.py for local packages

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Shreyash",
+    author_email="shrey.jiwane09@gmail.com",
+    packages=find_packages()
+)
